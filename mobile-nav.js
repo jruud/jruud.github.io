13f9bd979198569965cb5b57fbbdd6dcb78497fd
Josh Ruud <joshruud@joshs-mbp.lan>
2018-03-22 00:45:03 -0400

    First commit

---
+++ mobile-nav.js
@@ -0,0 +1,9 @@
+$(document).ready( function(){
+
+  $("#mobile-nav-button").click( function() {
+    $("#nav-wrapper").slideToggle();
+    $("#arrow-down").toggle();
+    $("#arrow-up").toggle();
+
+  });
+});
