b82d3f2cd4c410acc23ecfafde7c0d2abd5f5a5d
Josh Ruud <joshruud@joshs-mbp.lan>
2018-03-26 23:03:02 -0400

    Added universal footer

--- styles.js
+++ styles.js
@@ -1,8 +1,15 @@
+
+
 $(document).ready( function() {
+
+  //Change content h2 color to white on mouse enter
+
   $(".content-square").on('mouseenter', function() {
     $("h2", this).css("color", "white");
   });
 
+  //Revert h2 color to inherit on mouse leave
+  
   $(".content-square").on('mouseleave', function() {
     $("h2", this).css("color", "inherit");
   });
